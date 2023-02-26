6056c4ba7821b60f8390c3a85ec087ccdafe1d3e
ori metzer <126154081+ori-metzer@users.noreply.github.com>
2023-02-26 13:45:32 +0200

    Add files via upload

---
+++ Snake.java
@@ -0,0 +1,13 @@
+
+
+public class Snake {
+    private int length;
+    private Square squareNumber;
+    public Snake(int length,Square squareNumber){
+        this.length=length;
+        this.squareNumber= squareNumber;
+    }
+    public int getLength(){ return length; }
+    public Square getSquareNumber(){ return squareNumber;}
+
+}
